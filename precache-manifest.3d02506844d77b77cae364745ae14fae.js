7b009027fe6bd21e8923dae6df2ea82c271b7e9b
divagarv <@divagar.v@gmail.com>
2018-10-03 17:44:50 +0530

    Updates

---
+++ precache-manifest.3d02506844d77b77cae364745ae14fae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d8f268a5dd3653012bae9550228e99d",
+    "url": "/b2r-leaderboard/static/media/logo1.8d8f268a.svg"
+  },
+  {
+    "revision": "fe858d61c8dc62cd598b",
+    "url": "/b2r-leaderboard/static/js/runtime~main.fe858d61.js"
+  },
+  {
+    "revision": "1f7c38c6bb3cc47bbe58",
+    "url": "/b2r-leaderboard/static/js/main.1f7c38c6.chunk.js"
+  },
+  {
+    "revision": "eb63ff9ed6933118a68f",
+    "url": "/b2r-leaderboard/static/js/1.eb63ff9e.chunk.js"
+  },
+  {
+    "revision": "1f7c38c6bb3cc47bbe58",
+    "url": "/b2r-leaderboard/static/css/main.9ae352ff.chunk.css"
+  },
+  {
+    "revision": "eb63ff9ed6933118a68f",
+    "url": "/b2r-leaderboard/static/css/1.e89f0dd0.chunk.css"
+  },
+  {
+    "revision": "c8ba261603ad5d1a1627da7f6c30cd2b",
+    "url": "/b2r-leaderboard/index.html"
+  }
+];
